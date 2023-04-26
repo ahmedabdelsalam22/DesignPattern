c23a3723ec2ebebeebbf3633a0c20e65d18480c1
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-26 04:39:46 +0200

    add prototype

---
+++ Software_Design_Patterns/Prototype/EmployeePrototype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Design_Patterns.Prototype
+{
+    public abstract class EmployeePrototype
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public Address EmpAddress { get; set; }
+
+        public abstract EmployeePrototype ShallowCopy();
+        public abstract EmployeePrototype DeepCopy();
+
+        public override string ToString()
+        {
+            return 
+             $@"
+               Id: {this.Id},
+               Name: {this.Name},
+               Address:{this.EmpAddress.Building},{this.EmpAddress.StreetName},{this.EmpAddress.City}
+             ";
+        }
+    }
+}
